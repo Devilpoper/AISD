edcc49ded8eb8d32fb280220413bcbc05190a975
Devilpoper <mengee101@gmail.com>
2024-10-18 12:07:43 +0300

    added func travel of bin tree

--- min-heap.py
+++ min-heap.py
@@ -1,11 +1,11 @@
-from heapq import heapify
-
-
 class MinHeap:
-    def __init__(self, arr):
-        self.arr = arr
-        for i in range(len(arr) // 2, -1, -1):
-            self.heapify(i)
+    def __init__(self, array):
+        self.post_travel_list = []
+        self.pre_travel_list = []
+        self.in_travel_list = []
+        self.arr = array
+        for elem in range(len(array) // 2, -1, -1):
+            self.heapify(elem)
 
     def heapify(self, idx):
         while True:
@@ -13,10 +13,10 @@ class MinHeap:
             right_child = (2 * idx) + 2
             largest_child = idx
 
-            if left_child < len(self.arr) and self.arr[left_child] > self.arr[largest_child]:
+            if left_child < len(self.arr) and self.arr[left_child] < self.arr[largest_child]:
                 largest_child = left_child
 
-            if right_child < len(self.arr) and self.arr[right_child] > self.arr[largest_child]:
+            if right_child < len(self.arr) and self.arr[right_child] < self.arr[largest_child]:
                 largest_child = right_child
 
             if largest_child == idx:
@@ -29,17 +29,34 @@ class MinHeap:
         return " ".join(list(map(str, self.arr)))
 
     def print(self):
-        for i in range(0, len(arr)//2):
-            left = 2 * i + 1
-            right = 2 * i + 2
-            print(f"root: {arr[i]} ", end="")
-            if left < len(arr):
-                print(f"left: {arr[left]}")
-            if right < len(arr):
-                print(f"    right: {arr[right]}")
+        for root in range(0, len(self.arr) // 2):
+            left = 2 * root + 1
+            right = 2 * root + 2
+            print(f"root: {self.arr[root]} ", end="")
+            if left < len(self.arr):
+                print(f"left: {self.arr[left]}")
+            if right < len(self.arr):
+                print(f"    right: {self.arr[right]}")
+
+    def in_order_travel(self, idx_root):
+        if idx_root >= len(self.arr):
+            return
+        self.in_order_travel((idx_root * 2) + 1)
+        self.in_travel_list.append(self.arr[idx_root])
+        self.in_order_travel((idx_root * 2) + 2)
 
 
-arr = [1, 2, 3, 4, 5, 6, 7, 8]
-heap = MinHeap(arr)
-print(heap)
-heap.print()
+    def pre_order_travel(self, idx_root):
+        if idx_root >= len(self.arr):
+            return
+        self.pre_travel_list.append(self.arr[idx_root])
+        self.pre_order_travel((idx_root * 2) + 1)
+        self.pre_order_travel((idx_root * 2) + 2)
+
+
+    def post_order_travel(self, idx_root):
+        if idx_root >= len(self.arr):
+            return
+        self.post_order_travel(idx_root * 2 + 1)
+        self.post_order_travel(idx_root * 2 + 2)
+        self.post_travel_list.append(self.arr[idx_root])
