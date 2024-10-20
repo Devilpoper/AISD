15805ae4ccaafe86b20ee220c32924c79d1cf0bb
Devilpoper <mengee101@gmail.com>
2024-10-20 14:07:51 +0300

    add avl realisation file

---
+++ avl-tree.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.height: int = 1
+
+
+class AVLTree:
+    def __init__(self, root: Node):
+        self.root = root
+
+    def insert(self):
+        pass
+
+    def remove(self):
+        pass
